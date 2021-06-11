e6069f80b7dddab5d92684abe47f809118f1a391
sergiosssg <sergiosssg@gmail.com>
2021-06-11 15:19:19 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NUnit_for_PreprocessorMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
